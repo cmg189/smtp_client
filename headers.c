47301a1eb5ddf2daf625bdf96544a26b402e894c
cmg189 <tx189@yahoo.com>
2022-10-19 14:07:42 -0500

    get_email_details() implemented

--- headers.c
+++ headers.c
@@ -11,3 +11,30 @@ void check_exe(int num_args){
 
     return;
 }
+
+// get email details from user
+struct Email_info get_email_details(){
+    struct Email_info email; 
+
+    printf("\nProvide the details for the email you are sending\n\n");
+
+    printf("Sender's name: ");
+    scanf("%[^\n]%*c", email.senders_name);
+
+    printf("Sender's email address: ");
+    scanf("%[^\n]%*c", email.source);
+
+    printf("\nRecipient's name: ");
+    scanf("%[^\n]%*c", email.recipients_name);
+
+    printf("Recipient's email address: ");
+    scanf("%[^\n]%*c", email.destination);
+
+    printf("\nEmail subject: ");
+    scanf("%[^\n]%*c", email.subject);
+
+    printf("Email body:\n");
+    scanf("%[^\n]%*c", email.body);
+
+    return email;
+}
