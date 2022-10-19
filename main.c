6d106eb55cb225328c2f410ee986348802da5fb8
cmg189 <tx189@yahoo.com>
2022-10-19 14:05:29 -0500

    implemented get_email_details()

--- main.c
+++ main.c
@@ -6,6 +6,10 @@ int main(int argc, char *argv[]){
     // check if program is executed with email and password
     check_exe(argc);
 
+    // get email info from user
+    struct Email_info email = get_email_details();
+    
+
     printf("\nProgram ended\n\n");
     return 0;
 }
