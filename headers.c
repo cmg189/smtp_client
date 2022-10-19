e88d48b39bd14dafb9985a542d49a535d070d9ff
cmg189 <tx189@yahoo.com>
2022-10-19 12:12:48 -0500

    check_exe() added to ensure exe is done correctly

--- headers.c
+++ headers.c
@@ -1,1 +1,13 @@
 #include "headers.h"
+
+
+// check if program is executed with email and password
+void check_exe(int num_args){
+
+    if(num_args != EXE_ARGS){
+        printf("\nUsage: email_client your@email.com password\n\nProgram ended\n\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return;
+}
--- headers.h
+++ headers.h
@@ -1,2 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define EXE_ARGS 3
+
+// check if program is executed with email and password
+void check_exe(int num_args);
 
