bb4ba492fb37455f0f5b408a7cf6b0629752650a
cmg189 <tx189@yahoo.com>
2022-10-26 15:58:07 -0500

    base64_encode() for username and password

--- main.c
+++ main.c
@@ -6,15 +6,17 @@ int main(int argc, char *argv[]){
     char smtp_server[] = "mail.smtp2go.com";    // hostname of smtp2go
     int smtp_port = 2525;   // port# used by smtp2go
 
-    // check if program is executed with email and password
-    check_exe(argc);
 
-    // get email info from user
-    struct Email_info email = get_email_details();
+    check_exe(argc);        // check if program is executed with email and password
+
+    struct Email_info email = get_email_details();      // get email info from user
     
-    int server_socket_fd = connect_to_server(smtp_server, smtp_port);
+    int server_socket_fd = connect_to_server(smtp_server, smtp_port);   // create tcp socket to smtp2go
 
-    authenicate_account(server_sock_fd);
+    char* encoded_username = base64_encode(argv[1]);        // encode username to base64
+    char* encoded_password = base64_encode(argv[2]);        // encode password to base64
+   
+    authenticate_account(server_socket_fd, encoded_username, encoded_password);    // verify account is valid with smtp2go
 
     printf("\nProgram ended\n\n");
     return 0;
