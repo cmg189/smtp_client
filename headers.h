4be79a2e88f81bebabf50432070e382cfa29a7bb
cmg189 <tx189@yahoo.com>
2022-10-26 13:47:24 -0500

    libraries and definitions

--- headers.h
+++ headers.h
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
 #define EXE_ARGS 3
 #define EMAIL_SIZE 30
 #define SUBJECT_SIZE 50
 #define BODY_SIZE 10000
+#define _5BYTES 5120
+#define _1BYTE 1024
+#define _1KB 8192
 
 // holds details of email from user
 struct Email_info{
@@ -23,4 +32,4 @@ void check_exe(int num_args);
 struct Email_info get_email_details();
 
 // connect to smtp2go server
-int connect_to_server(smtp_server, smtp_port);
+int connect_to_server(char smtp_server[], int smtp_port);
