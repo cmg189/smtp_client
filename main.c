7f3a88a0147724b909baa5ddd7285ef9a75789c1
cmg189 <tx189@yahoo.com>
2022-10-26 12:45:16 -0500

    hostname and port# added

--- main.c
+++ main.c
@@ -3,12 +3,16 @@
 
 int main(int argc, char *argv[]){
 
+    char smtp_server[] = "mail.smtp2go.com";    // hostname of smtp2go
+    int smtp_port = 2525;   // port# used by smtp2go
+
     // check if program is executed with email and password
     check_exe(argc);
 
     // get email info from user
     struct Email_info email = get_email_details();
     
+    int server_socket_fd = connect_to_server(smtp_server, smtp_port);
 
     printf("\nProgram ended\n\n");
     return 0;
